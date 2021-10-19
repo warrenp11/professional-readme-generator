5ff273aed97fc8ce2ba094a936b788cd76597055
Peter <warrenp11@gmail.com>
2021-10-19 19:37:41 -0400

    add basic readme.md skeleton

--- index.js
+++ index.js
@@ -1,91 +1,95 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'github',
-        message: 'what is your github username? (required)',
-        validate: username => {
-            if(username) {
-                return true;
-            } else {
-                console.log('you must enter a github username!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'what is your email address? (required)',
-        validate: email => {
-            if(email) {
-                return true;
-            } else {
-                console.log('you must enter an email address!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'project',
-        message: 'what is your project name? (required)',
-        validate: project => {
-            if(project) {
-                return true;
-            } else {
-                console.log('you must enter a project name!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'provide a description for your project'
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: 'what are the steps required to install your project?'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'provide instructions and examples for use'
-    },
-    {
-        type: 'input',
-        name: 'credit',
-        message: 'list collaborators and third-party assets, if any'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'what command should be run for tests?',
-        // default: 'npm index'
-    },
-    {
-        type: 'input',
-        name: 'contribute',
-        message: 'what do users need to know about contributing to repo?'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'what licenses does your project have?',
-        choices: ['MIT', 'Driver\'s', 'Other']
-    }
-    ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    console.log(answers);
-  })
-const questions = [];
+const questions = () => {
+    return inquirer
+    .prompt([
+      {
+          type: 'input',
+          name: 'github',
+          message: 'what is your github username? (required)',
+          validate: username => {
+              if(username) {
+                  return true;
+              } else {
+                  console.log('you must enter a github username!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'email',
+          message: 'what is your email address? (required)',
+          validate: email => {
+              if(email.includes('@')) {
+                  return true;
+              } else {
+                  console.log('you must enter an email address!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'project',
+          message: 'what is your project name? (required)',
+          validate: project => {
+              if(project) {
+                  return true;
+              } else {
+                  console.log('you must enter a project name!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'description',
+          message: 'provide a description for your project'
+      },
+      {
+          type: 'input',
+          name: 'install',
+          message: 'what are the steps required to install your project?'
+      },
+      {
+          type: 'input',
+          name: 'usage',
+          message: 'provide instructions and examples for use'
+      },
+      {
+          type: 'input',
+          name: 'credit',
+          message: 'list collaborators and third-party assets, if any'
+      },
+      {
+          type: 'input',
+          name: 'tests',
+          message: 'what command should be run for tests?',
+          // default: 'npm index'
+      },
+      {
+          type: 'input',
+          name: 'contribute',
+          message: 'what do users need to know about contributing to repo?'
+      },
+      {
+          type: 'list',
+          name: 'license',
+          message: 'what licenses does your project have?',
+          choices: ['MIT', 'Apache', 'GPL', 'BSD', 'Unlicensed', 'Other']
+      }
+      ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+      console.log(answers);
+    })
+};
+
+questions();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,9 +11,33 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = data => {
   return `# ${data.title}
 
+  ## Description
+  ${`description`}
+
+  ## Table of Contents
+  ## Installation
+  ${`install`}
+
+  ## Usage
+  ${`usage`}
+
+  ## Credits
+  ${`credit`}
+
+  ## Tests
+  ${`tests`}
+
+  ## Contributing
+  ${`contribute`}
+
+  ## Questions
+  If you have any questions about this project, contact me directly at ${`email`}. You can view more of my projects at ${`github`}.
+  
+  ## License
+  Licensed under the ${`license`} license.
 `;
 }
 
