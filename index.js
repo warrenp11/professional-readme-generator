ee538ee300d386e9d9b7e8cb77abb1643f05a5e5
Peter <warrenp11@gmail.com>
2021-10-22 00:10:48 -0400

    deleted unnecessary code

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
-const renderLicenseBadge = require('./utils/generateMarkdown.js');
-const { clearLine } = require('readline');
-const { clear } = require('console');
 
 // TODO: Create an array of questions for user input
 const questions = () => {
